e023a3b4b344f73cc5f84bcb6a55051602ea2d9f
Christopher Langner <langnerc@icloud.com>
2022-05-08 07:44:18 -0500

    created MVP API calls

--- server.js
+++ server.js
@@ -48,6 +48,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -57,4 +62,13 @@ app.get("/api/animals", (req, res) => {
 });
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
